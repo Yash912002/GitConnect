535c6d6d776ba8c745cf55afed28c7e410c4be45
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-03-09 13:31:23 +0530

    Add API route to fetch GitHub repository information

---
+++ src/app/api/github/[repo_name]/route.ts
@@ -0,0 +1,62 @@
+import { NextRequest, NextResponse } from "next/server";
+import { GITHUB_API_URL } from "../route";
+import { getToken } from "@/utils/getToken";
+
+export async function GET(
+	req: NextRequest,
+	{ params }: { params: { repo_name: string } }
+) {
+	const token = getToken();
+	if (token instanceof Response) return token;
+
+	const { repo_name: repositoryName } = params;
+
+	if (!repositoryName) {
+		return NextResponse.json(
+			{ error: "Repository name is missing" },
+			{ status: 400 }
+		);
+	}
+
+	try {
+		const repoInformation = await fetch(
+			`${GITHUB_API_URL}/repos/Yash912002/${repositoryName}`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+					Accept: "application/vnd.github+json",
+				},
+			}
+		);
+
+		if (!repoInformation.ok) {
+			return NextResponse.json(
+				{
+					error: "Repository not found",
+				},
+				{ status: repoInformation.status }
+			);
+		}
+
+		const repository = await repoInformation.json();
+
+		const formattedResponse = {
+			name: repository.name,
+			description: repository.description,
+			stars: repository.stargazers_count,
+			forks: repository.forks_count,
+			open_issues: repository.open_issues_count,
+			url: repository.html_url,
+		};
+
+		return NextResponse.json(formattedResponse);
+	} catch (error) {
+		console.error("Error fetching repository data:", error);
+		return NextResponse.json(
+			{
+				error: "Internal server error while fetching repository data.",
+			},
+			{ status: 500 }
+		);
+	}
+}
