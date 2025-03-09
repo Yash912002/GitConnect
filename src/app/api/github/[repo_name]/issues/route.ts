3e35362d494a7e2061534470995bc26f7bd9d293
Yash Bhavsar <yashbhavsar02@gmail.com>
2025-03-09 13:31:34 +0530

    Add API route to create GitHub issues

---
+++ src/app/api/github/[repo_name]/issues/route.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from "next/server";
+import { GITHUB_API_URL } from "../../route";
+import { getToken } from "@/utils/getToken";
+
+export async function POST(
+	req: NextRequest,
+	{ params }: { params: { repo_name: string } }
+) {
+	const token = getToken();
+	if (token instanceof Response) return token;
+
+	const { repo_name: repositoryName } = params;
+
+	if (!repositoryName) {
+		return NextResponse.json(
+			{ error: "Repository name is missing" },
+			{ status: 400 }
+		);
+	}
+
+	const { title, body } = await req.json();
+
+	if (!title || !body) {
+		return NextResponse.json(
+			{
+				error: "Both title and body are required to create an issue",
+			},
+			{ status: 400 }
+		);
+	}
+
+	try {
+		const createIssue = await fetch(
+			`${GITHUB_API_URL}/repos/Yash912002/${repositoryName}/issues`,
+			{
+				method: "POST",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					Accept: "application/vnd.github+json",
+				},
+				body: JSON.stringify({
+					title,
+					body,
+				}),
+			}
+		);
+
+		if (!createIssue.ok) {
+			return NextResponse.json(
+				{
+					error: "Failed to create issue",
+				},
+				{ status: createIssue.status }
+			);
+		}
+
+		const issue_data = await createIssue.json();
+
+		const response = {
+			issue_url: issue_data.html_url,
+		};
+
+		return NextResponse.json(response);
+	} catch (error) {
+		console.error("Error creating GitHub issue:", error);
+		return NextResponse.json(
+			{
+				error: "Internal server error while creating issue.",
+			},
+			{ status: 500 }
+		);
+	}
+}
